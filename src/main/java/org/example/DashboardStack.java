508102875bceb3dba843aa3b19b670be2fd07fa2
Marat Razzivin <marat061206@gmail.com>
2025-03-24 20:47:03 +0200

    added design (and its logic) of transaction filters, small changes to dashboard.fxml and new classes in css

--- src/main/java/org/example/DashboardStack.java
+++ src/main/java/org/example/DashboardStack.java
@@ -1,8 +1,12 @@
 package org.example;
 
 import enums.TransactionType;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import models.Transaction;
 import models.User;
 import services.Database;
@@ -18,6 +22,24 @@ public class DashboardStack {
     Label moneyIn;
     @FXML
     Label moneyOut;
+    @FXML
+    TextField searchField;
+    @FXML
+    Button sortByDate;
+    @FXML
+    Button sortByPrice;
+    @FXML
+    ImageView priceArrow;
+    @FXML
+    ImageView dateArrow;
+
+    @FXML
+    private void initialize() {
+        sortByDate.setOnAction(this::dateButton);
+        sortByPrice.setOnAction(this::priceButton);
+        changeToDate();
+    }
+
     public void setUser(User user) {
         currentUser = user;
         setBalanceValue();
@@ -30,4 +52,63 @@ public class DashboardStack {
     }
     public void setMoneyIn() { moneyIn.setText(db.moneyIn(currentUser).toString() + "€");}
     public void setMoneyOut() { moneyOut.setText(db.moneyOut(currentUser).toString() + "€");}
+
+    public void dateButton(ActionEvent event) { changeToDate(); }
+    public void priceButton(ActionEvent event) { changeToPrice(); }
+
+    public void changeToDate() {
+        if (sortByDate.getStyleClass().getFirst().equals("activeButton")) {
+            if (dateArrow.getStyleClass().getFirst().equals("arrowDown")) {
+                dateUp();
+            } else {
+                dateDown();
+            }
+        } else {
+            dateDown();
+        }
+
+        sortByDate.getStyleClass().clear();
+        sortByDate.getStyleClass().add("activeButton");
+        dateArrow.getStyleClass().add("activeArrow");
+
+        sortByPrice.getStyleClass().clear();
+        sortByPrice.getStyleClass().add("inactiveButton");
+        priceUp();
+        priceArrow.getStyleClass().add("inactiveArrow");
+    }
+    public void changeToPrice() {
+        if (sortByPrice.getStyleClass().getFirst().equals("activeButton")) {
+            if (priceArrow.getStyleClass().getFirst().equals("arrowDown")) {
+                priceUp();
+            } else {
+                priceDown();
+            }
+        } else {
+            priceUp();
+        }
+
+        sortByPrice.getStyleClass().clear();
+        sortByPrice.getStyleClass().add("activeButton");
+        priceArrow.getStyleClass().add("activeArrow");
+
+        sortByDate.getStyleClass().clear();
+        sortByDate.getStyleClass().add("inactiveButton");
+        dateDown();
+        dateArrow.getStyleClass().add("inactiveArrow");
+    }
+
+    public void dateDown() {
+        dateArrow.getStyleClass().clear();
+        dateArrow.getStyleClass().add("arrowDown");
+    }
+    public void dateUp() {
+        dateArrow.getStyleClass().clear();
+    }
+    public void priceDown() {
+        priceArrow.getStyleClass().clear();
+        priceArrow.getStyleClass().add("arrowDown");
+    }
+    public void priceUp() {
+        priceArrow.getStyleClass().clear();
+    }
 }
